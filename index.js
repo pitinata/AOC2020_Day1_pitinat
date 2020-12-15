554f445f3fc061dc6930902e9084b1038eee5da5
pitinat <pitinat@gmail.com>
2020-12-15 22:51:57 +0700

    - Change input array to file reading system - Move all function to be working inside the readFile function

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-let dataArr = [1721,979,366,299,675,1722];
-let pairResult = -1;
+const fs = require('fs');
 
-for(const num of dataArr){
-    if(dataArr.includes(2020-num)){
-        pairResult = num;
-        break;
+fs.readFile('./input/day1_input.txt', 'utf-8', (err,data) => {
+    const dataArr = data.split('\n');
+    let pairResult = -1;
+
+    const dataNumberArr = dataArr.map(data => Number(data));
+
+    for(const num of dataNumberArr){
+        if(dataNumberArr.includes(2020-num)){
+            pairResult = num;
+            break;
+        }
     }
-}
+    
+    if(pairResult > -1){
+        const pairResult_inv = 2020-pairResult;
+        console.log(pairResult+' x '+(pairResult_inv)+' = '+(pairResult*pairResult_inv));
+    }
+    else{
+        console.log('No pair found');
+    }
 
-if(pairResult > -1){
-    const pairResult_inv = 2020-pairResult;
-    console.log(pairResult+' x '+(pairResult_inv)+' = '+(pairResult*pairResult_inv));
-}
-else{
-    console.log('No pair found');
-}
+});
+
+
+
+
+
+
 
 
 
