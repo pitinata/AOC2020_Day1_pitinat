c38929f4cf464f7e16af16210c85cdb0dcc90d7d
pitinat <pitinat@gmail.com>
2020-12-15 22:56:35 +0700

    Put all code in try, catch block

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const fs = require('fs');
 
 fs.readFile('./input/day1_input.txt', 'utf-8', (err,data) => {
-    const dataArr = data.split('\n');
-    let pairResult = -1;
+    
+    try{
+        const dataArr = data.split('\n');
+        let pairResult = -1;
 
-    const dataNumberArr = dataArr.map(data => Number(data));
+        const dataNumberArr = dataArr.map(data => Number(data));
 
-    for(const num of dataNumberArr){
-        if(dataNumberArr.includes(2020-num)){
-            pairResult = num;
-            break;
+        for(const num of dataNumberArr){
+            if(dataNumberArr.includes(2020-num)){
+                pairResult = num;
+                break;
+            }
+        }
+        
+        if(pairResult > -1){
+            const pairResult_inv = 2020-pairResult;
+            console.log(pairResult+' x '+(pairResult_inv)+' = '+(pairResult*pairResult_inv));
+        }
+        else{
+            console.log('No pair found');
         }
     }
+    catch(err){
+        console.log("Oops! There is an error: ", err)
+    };
     
-    if(pairResult > -1){
-        const pairResult_inv = 2020-pairResult;
-        console.log(pairResult+' x '+(pairResult_inv)+' = '+(pairResult*pairResult_inv));
-    }
-    else{
-        console.log('No pair found');
-    }
+})
 
-});
 
 
 
